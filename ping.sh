3dce32db42ebfb7ebf5fd95f631a14def52827a6
roberto-farias <67426067+roberto-farias@users.noreply.github.com>
2022-06-27 21:43:31 -0300

    Update ping.sh

--- ping.sh
+++ ping.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-ping ()
+ping()
 	{
-		iptables -A INPUT -p icmp --icmp-type 0 -s 0/0 -d 10.0.2.15 -j ACCEPT
-		iptables -A OUTPUT -p icmp --icmp-type 8 -s 10.0.2.15 -d 0/0 -j ACCEPT
+		iptables -A INPUT -p icmp --icmp-type 0 -s 0/0 -d 10.0.2.15/32 -j ACCEPT
+		iptables -A OUTPUT -p icmp --icmp-type 8 -s 10.0.2.15/32 -d 0/0 -j ACCEPT
+
+		iptables -A INPUT -p icmp --icmp-type 8 -s 0/0 -d 10.0.2.15/32 -j ACCEPT
+		iptables -A OUTPUT -p icmp --icmp-type 0 -s 10.0.2.15/32 -d 0/0 -j ACCEPT
 	}
 ping

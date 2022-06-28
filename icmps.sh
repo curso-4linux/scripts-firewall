2ab98a4e38f9cc864fa830c2b7b7c38afc0d7e50
roberto-farias <67426067+roberto-farias@users.noreply.github.com>
2022-06-27 21:45:54 -0300

    Update icmps.sh

--- icmps.sh
+++ icmps.sh
@@ -3,8 +3,8 @@ icmps()
 	{
 		for tipo in 0 3/0 3/1 3/2 3/3 3/4 4 5 11 12
 		do
-			iptables -A INPUT -p icmp -s 0/0 -d 10.0.2.15 --icmp-type $tipo -m limit --limit 1/s -j ACCEPT
+		iptables -A INPUT -p icmp -s 0/0 -d 10.0.2.15/32 --icmp-type $tipo -m limit --limit 1/s -j ACCEPT
 		done
-		iptables -A OUTPUT -p icmp --icmp-type 8 -s 10.0.2.15 -d 0/0 -j ACCEPT
+		iptables -A OUTPUT -p icmp --icmp-type 8 -s 10.0.2.15/32 -d 0/0 -j ACCEPT
 	}
 icmps
